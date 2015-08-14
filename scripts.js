62a33bd3d87132760c14a2a36ea7f649f7530230
Ryan M <rm.main@gmail.com>
2015-08-14 18:33:20 -0500

    Updated all my files and they worked.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,4 @@
-console.log("Hello, World!  There is text in my console.");
+function welcome(){
+  document.write("<p>Woot! It worked!</p>");
+  console.log("Hello, World!  There is text in my console.");
+}
